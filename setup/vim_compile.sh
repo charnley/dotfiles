2185735c2a465411448f86d5d21e068182de6e54
Jimmy Charnley Kromann <jimmy@charnley.dk>
2020-05-10 14:30:31 +0200

    fix and doc

--- setup/vim_compile.sh
+++ setup/vim_compile.sh
@@ -4,27 +4,28 @@ configdir=`python3-config --configdir`
 prefix=$HOME/opt/vim
 install_dir=vim_build
 
-# cd $HOME/opt
-#
-# git clone https://github.com/vim/vim.git $install_dir --depth 1
-#
-# cd $install_dir
-#
+
+# Switch folder and clone
+cd $HOME/opt
+git clone https://github.com/vim/vim.git $install_dir --depth 1
+cd $install_dir
 
 # Needs to have a python3 env activated
-# TODO Check that this was done succesfully
 source $CONDA_PREFIX/etc/profile.d/conda.sh
 conda activate
 
-# Clean up after yourself
+# TODO if conda activate fails; exit
+
+# Ensure clean build
 make distclean
 
-# TODO
+# TODO Ensure and enable other interps
 # --enable-rubyinterp=yes \
 # --enable-perlinterp=yes \
 # --enable-luainterp=yes \
 # --enable-gui=gtk2 \
 
+# Configure build
 ./configure --with-features=huge \
     --enable-cscope \
     --enable-multibyte \
@@ -32,7 +33,9 @@ make distclean
     --with-python3-config-dir=$configdir \
     --prefix=$HOME/opt/vim
 
+# Compile and link
 make
 
+# install to prefix
 make install
 
