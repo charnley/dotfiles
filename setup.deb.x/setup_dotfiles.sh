c077f980977138ae3a7af3c6893efe30216cf09f
Jimmy Charnley Kromann <jimmy@charnley.dk>
2021-12-03 09:57:18 +0100

    hpc is by default deb

--- setup.deb.x/setup_dotfiles.sh
+++ setup.deb.x/setup_dotfiles.sh
@@ -11,11 +11,19 @@ ln -sf $cwd/dot/bash_profile ~/.bash_profile
 ln -sf $cwd/dot/inputrc ~/.inputrc
 ln -sf $cwd/dot/tmux.conf ~/.tmux.conf
 ln -sf $cwd/dot/condarc ~/.condarc
+ln -sf $cwd/dot/gitconfig ~/.gitconfig
+
+# i3
+ln -sf $cwd/dot.deb.x/Xresources ~/.Xresources
+ln -sf $cwd/dot.deb.x/i3status ~/.i3status
+ln -sf $cwd/dot.deb.x/i3config ~/.i3/config
 
 # .ssh
 mkdir -p ~/.ssh
 ln -sf $cwd/dot/ssh_config ~/.ssh/config
 
 # .config
+ln -sf $cwd/dot.deb.x/dunstrc ~/.config/dunstrc
+mkdir -p ~/.config/nvim 
 ln -sf $cwd/dot/neovim/init.vim ~/.config/nvim/init.vim
 
