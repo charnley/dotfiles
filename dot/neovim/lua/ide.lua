947ee83f41f1f37f73c89b32cb092623a960c521
Jimmy Charnley Kromann <jimmy@charnley.dk>
2021-12-07 23:30:41 +0100

    ide settings

---
+++ dot/neovim/lua/ide.lua
@@ -0,0 +1,242 @@
+require('lspconfig').pyright.setup{}
+require('lspconfig').bashls.setup{}
+require('nvim-autopairs').setup{}
+
+
+-- Setup nvim-cmp.
+local cmp = require'cmp'
+
+-- auto bracket on cmp complete
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
+
+-- setup completion
+cmp.setup({
+snippet = {
+  -- REQUIRED - you must specify a snippet engine
+  expand = function(args)
+	vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+	-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+	-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+	-- require'snippy'.expand_snippet(args.body) -- For `snippy` users.
+  end,
+},
+mapping = {
+  ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+  ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+  ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+  ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+  ['<C-e>'] = cmp.mapping({
+	i = cmp.mapping.abort(),
+	c = cmp.mapping.close(),
+  }),
+  -- Accept currently selected item. If none selected, `select` first item.
+  -- Set `select` to `false` to only confirm explicitly selected items.
+  ['<CR>'] = cmp.mapping.confirm({ select = true }),
+},
+sources = cmp.config.sources({
+  { name = 'nvim_lsp' },
+  { name = 'vsnip' }, -- For vsnip users.
+}, {
+  { name = 'buffer' },
+})
+})
+
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+-- cmp.setup.cmdline('/', {
+--   sources = {
+--     { name = 'buffer' }
+--   }
+-- })
+
+
+-- lsp setup
+-- Set Default Prefix.
+-- Note: You can set a prefix per lsp server in the lv-globals.lua file
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    virtual_text = {
+      -- prefix = "",
+      spacing = 0,
+    },
+    signs = true,
+    underline = true,
+  }
+)
+
+-- -- uncomment below to enable nerd-font based icons for diagnostics in the
+-- -- gutter, see:
+-- -- https://github.com/neovim/nvim-lspconfig/wiki/UI-customization#change-diagnostic-symbols-in-the-sign-column-gutter
+-- local signs = { Error = " ", Warning = " ", Hint = " ", Information = " " }
+--
+-- for type, icon in pairs(signs) do
+--   local hl = "LspDiagnosticsSign" .. type
+--   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+-- end
+
+-- symbols for autocomplete
+vim.lsp.protocol.CompletionItemKind = {
+    -- "   (Text) ",
+    -- "   (Method)",
+    -- "   (Function)",
+    -- "   (Constructor)",
+    -- " ﴲ  (Field)",
+    -- "[] (Variable)",
+    -- "   (Class)",
+    -- " ﰮ  (Interface)",
+    -- "   (Module)",
+    -- " 襁 (Property)",
+    -- "   (Unit)",
+    -- "   (Value)",
+    -- " 練 (Enum)",
+    -- "   (Keyword)",
+    -- "   (Snippet)",
+    -- "   (Color)",
+    -- "   (File)",
+    -- "   (Reference)",
+    -- "   (Folder)",
+    -- "   (EnumMember)",
+    -- " ﲀ  (Constant)",
+    -- " ﳤ  (Struct)",
+    -- "   (Event)",
+    -- "   (Operator)",
+    -- "   (TypeParameter)"
+}
+
+local function documentHighlight(client, bufnr)
+    -- Set autocommands conditional on server_capabilities
+    if client.resolved_capabilities.document_highlight then
+        vim.api.nvim_exec(
+            [[
+      hi LspReferenceRead cterm=bold ctermbg=red guibg=#464646
+      hi LspReferenceText cterm=bold ctermbg=red guibg=#464646
+      hi LspReferenceWrite cterm=bold ctermbg=red guibg=#464646
+      augroup lsp_document_highlight
+        autocmd! * <buffer>
+        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+      augroup END
+    ]],
+            false
+        )
+    end
+end
+
+local lsp_config = {}
+
+function lsp_config.common_on_attach(client, bufnr)
+    documentHighlight(client, bufnr)
+end
+function lsp_config.tsserver_on_attach(client, bufnr)
+    lsp_config.common_on_attach(client, bufnr)
+    client.resolved_capabilities.document_formatting = false
+end
+
+-- local function setup_servers()
+--   require'lspinstall'.setup()
+--   local servers = require'lspinstall'.installed_servers()
+--   for _, server in pairs(servers) do
+--     require'lspconfig'[server].setup{}
+--   end
+-- end
+
+-- setup_servers()
+
+-- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
+-- require'lspinstall'.post_install_hook = function ()
+--   setup_servers() -- reload installed servers
+--   vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
+-- end
+
+--- Super Tab like
+
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local feedkey = function(key, mode)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
+local cmp = require('cmp')
+cmp.setup {
+
+  -- ... Your other configuration ...
+
+  mapping = {
+
+    -- ... Your other mappings ...
+
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif vim.fn["vsnip#available"](1) == 1 then
+        feedkey("<Plug>(vsnip-expand-or-jump)", "")
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+        feedkey("<Plug>(vsnip-jump-prev)", "")
+      end
+    end, { "i", "s" }),
+
+    -- ... Your other mappings ...
+
+  }
+
+  -- ... Your other configuration ...
+
+}
+
+
+
+--- Treesitter
+require'nvim-treesitter.configs'.setup {
+    -- ensure_installed can be "all" or a list of languages { "python", "javascript" }
+    ensure_installed = {
+        "python",
+        "bash",
+        "javascript",
+        -- "clojure",
+        -- "go"
+    },
+
+    highlight = { -- enable highlighting for all file types
+      enable = true, -- you can also use a table with list of langs here (e.g. { "python", "javascript" })
+    },
+    incremental_selection = {
+      enable = true,  -- you can also use a table with list of langs here (e.g. { "python", "javascript" })
+      disable = { "cpp", "lua" },
+      keymaps = {                       -- mappings for incremental selection (visual mappings)
+        init_selection = "gnn",         -- maps in normal mode to init the node/scope selection
+        node_incremental = "grn",       -- increment to the upper named parent
+        scope_incremental = "grc",      -- increment to the upper scope (as defined in locals.scm)
+        node_decremental = "grm",       -- decrement to the previous node
+      }
+    },
+    textobjects = {
+      -- These are provided by
+      select = {
+        enable = true,  -- you can also use a table with list of langs here (e.g. { "python", "javascript" })
+        keymaps = {
+          -- You can use the capture groups defined here:
+-- https://github.com/nvim-treesitter/nvim-treesitter-textobjects/blob/master/queries/c/textobjects.scm
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+          ["ab"] = "@block.outer",
+          ["ib"] = "@block.inner",
+          ["as"] = "@statement.outer",
+          ["is"] = "@statement.inner",
+        },
+      },
+    },
+}
+
