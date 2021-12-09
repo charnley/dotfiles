9304bdba7c2c29f327845ad379407dc49fb2ea7b
Jimmy Charnley Kromann <jimmy@charnley.dk>
2021-12-09 22:24:00 +0100

    space

--- dot/neovim/lua/ide.lua
+++ dot/neovim/lua/ide.lua
@@ -15,10 +15,10 @@ cmp.setup({
 snippet = {
   -- REQUIRED - you must specify a snippet engine
   expand = function(args)
-	vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-	-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-	-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-	-- require'snippy'.expand_snippet(args.body) -- For `snippy` users.
+    vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+    -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+    -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+    -- require'snippy'.expand_snippet(args.body) -- For `snippy` users.
   end,
 },
 mapping = {
@@ -27,8 +27,8 @@ mapping = {
   ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
   ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
   ['<C-e>'] = cmp.mapping({
-	i = cmp.mapping.abort(),
-	c = cmp.mapping.close(),
+    i = cmp.mapping.abort(),
+    c = cmp.mapping.close(),
   }),
   -- Accept currently selected item. If none selected, `select` first item.
   -- Set `select` to `false` to only confirm explicitly selected items.
