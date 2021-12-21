d7d0036b95a73e4ab9a050b4f478c8da1d26853b
Jimmy Charnley Kromann <jimmy@charnley.dk>
2021-12-21 12:46:05 +0100

    added macosx alternative for install

--- setup/conda_install.sh
+++ setup/conda_install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $HOME/opt/miniconda3.sh
+if [[ $(uname -s) == Linux ]]
+then
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $HOME/opt/miniconda3.sh
+else
+    # Darwin
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O $HOME/opt/miniconda3.sh
+fi
 
 cd $HOME/opt
-
 bash miniconda3.sh -b -p $HOME/opt/miniconda3
-
+rm miniconda3.sh
