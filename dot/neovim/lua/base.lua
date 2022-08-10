ad3c85b9daae1331e676c3593824ec4476f285d0
Jimmy Charnley Kromann <jimmy@charnley.dk>
2022-08-10 14:51:55 +0200

    Starting base.lua

---
+++ dot/neovim/lua/base.lua
@@ -0,0 +1,29 @@
+
+
+vim.opt.autoindent
+vim.opt.expandtab
+vim.opt.indentexpr=O
+vim.opt.shiftwidth=4
+vim.opt.smartindent
+vim.opt.smarttab
+vim.opt.softtabstop=0
+vim.opt.tabstop=4
+
+vim.opt.ignorecase -- Case-insensitive searching
+vim.opt.lazyredraw -- will buffer screen updates instead of updating all the time.:help 'ttyfast'
+vim.opt.list -- Highlight unwanted spaces
+vim.opt.listchars=tab:▸\ ,trail:·
+vim.opt.mouse=a
+vim.opt.nohlsearch -- I don't like to look at highlighted text
+vim.opt.noshowmode -- insert is already showing in lightline
+vim.opt.noswapfile
+vim.opt.nowrap -- Don't ever wordwrap my code
+vim.opt.number
+vim.opt.relativenumber
+vim.opt.scrolloff=8 -- I like cursor to be in center
+vim.opt.sidescroll=1
+vim.opt.sidescrolloff=15
+vim.opt.signcolumn=yes:1 -- always show sign column (bookmarks, gitgutter,..)
+vim.opt.smartcase -- if a pattern contains an uppercase letter, it is case sensitive
+
+
