3a7912ec337781e5c3fc35662df90399ad08ba42
Jimmy Charnley Kromann <jimmy@charnley.dk>
2022-09-30 09:10:15 -0400

    Added todo

--- setup/javascript_nvm.sh
+++ setup/javascript_nvm.sh
@@ -7,4 +7,5 @@ export NVM_DIR=$HOME/opt/nvm
 mkdir -p $NVM_DIR
 
 # Will install to $NVM_DIR
+# TODO Don't change bashrc
 curl -s -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
