c7756f4976d3d15eb877695dce42c0055f75f93d
Jimmy Charnley Kromann <jimmy@charnley.dk>
2023-04-23 12:38:44 +0200

    Trying to switch to lazy

--- dot/neovim/lua/plugins.lua
+++ dot/neovim/lua/plugins.lua
@@ -1,3 +1,54 @@
+-- require("lazy").setup({
+--   "folke/which-key.nvim",
+--   'dstein64/vim-startuptime',
+--  
+--   'MattesGroeger/vim-bookmarks',  -- Easy bookmark shortcuts
+--   'ap/vim-css-color',  -- Preview CSS Colors
+--   'chaoren/vim-wordmotion', -- Better word motion
+--   'justinmk/vim-sneak'  -- Jump to char
+--   {'junegunn/fzf', run= './install --all'},  -- Fuzzy find searching
+--   'junegunn/fzf.vim',  -- Fuzzy find searching
+--   'numToStr/Comment.nvim', -- For Commenting gcc & gc (Treesitter support)
+--   'gorkunov/smartpairs.vim', -- Extend visual selection of bracket
+
+--    -- Interface
+--    'nanotech/jellybeans.vim', -- colorscheme
+--    'vim-airline/vim-airline',
+--    'vim-airline/vim-airline-themes',
+--    'folke/which-key.nvim',  -- I can't remember what <leader><char> does
+
+--    -- IDE like
+--    'windwp/nvim-autopairs',
+--    'hrsh7th/cmp-buffer',
+--    'hrsh7th/cmp-nvim-lsp',
+--    'hrsh7th/cmp-path',
+--    'hrsh7th/cmp-vsnip',
+--    'hrsh7th/nvim-cmp',
+--    'hrsh7th/vim-vsnip',
+--    'kabouzeid/nvim-lspinstall',
+--    'neovim/nvim-lspconfig',
+--    {'nvim-treesitter/nvim-t,reesitter', run= ':TSUpdate'},
+--    'nvim-treesitter/nvim-treesitter-textobjects',
+--    'rafamadriz/friendly-snippets',
+--    'ray-x/lsp_signature.nvim',  --  https://github.com/ray-x/lsp_signature.nvim
+--    'danymat/neogen',  -- generate docstrings (with treesitter)
+
+--    'airblade/vim-gitgutter', -- Git indication
+--    'apzelos/blamer.nvim', -- Git blame line
+
+--    -- Svelte development
+--    'joosepalviste/nvim-ts-context-commentstring', -- for context comment
+--    'evanleck/vim-svelte',
+--    'pangloss/vim-javascript',
+--    'herringtondarkholme/yats.vim',
+
+--    -- Write mode
+--    'folke/zen-mode.nvim',
+--    'gaodean/autolist.nvim',
+-- })
+
+
+
 return require('packer').startup(function(use)
 
     -- Packer can manage itself
