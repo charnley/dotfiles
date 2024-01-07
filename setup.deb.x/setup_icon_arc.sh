b9d9293abb0906340e4a08d019717b40085abf05
Jimmy Charnley Kromann <jimmy@charnley.dk>
2024-01-07 19:29:11 +0100

    Getting that laptop setup

--- setup.deb.x/setup_icon_arc.sh
+++ setup.deb.x/setup_icon_arc.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+set -x
+set -e
+set -u
+
 mkdir -p ~/dwnl/
 cd ~/dwnl/
 
 git clone https://github.com/horst3180/arc-icon-theme --depth 1
 cd arc-icon-theme
 
-rsync -av Arc /usr/share/icons
+sudo rsync -av Arc /usr/share/icons
 
 cd ..
-rm -fr arc-icon-theme
-
