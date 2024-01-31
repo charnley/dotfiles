218057482160410935e5b823b95c909580579108
Jimmy Charnley Kromann <jimmy@charnley.dk>
2024-01-31 09:19:21 +0100

    Configure TreeSitter context

--- dot/neovim/lua/ide.lua
+++ dot/neovim/lua/ide.lua
@@ -223,7 +223,7 @@ require'treesitter-context'.setup{
   max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
   min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
   line_numbers = true,
-  multiline_threshold = 20, -- Maximum number of lines to show for a single context
+  multiline_threshold = 1, -- Maximum number of lines to show for a single context
   trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
   mode = 'cursor',  -- Line used to calculate context. Choices: 'cursor', 'topline'
   -- Separator between context and content. Should be a single character string, like '-'.
@@ -232,5 +232,19 @@ require'treesitter-context'.setup{
   zindex = 20, -- The Z-index of the context window
   on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
 }
+
+vim.api.nvim_exec([[
+hi TreesitterContextBottom gui=underline guisp=Grey
+hi TreesitterContextLineNumberBottom gui=underline guisp=Grey
+hi TreesitterContextLineNumberBottom gui=underline guisp=Grey
+]], false)
+
+vim.api.nvim_set_hl(0, "Normal", { ctermfg=White,  ctermbg=Black })
+-- vim.keymap.set("n", "[c", function()
+--   require("treesitter-context").go_to_context(vim.v.count1)
+-- end, { silent = true })
+vim.api.nvim_set_keymap("n", "<Leader>nc", ":TSContextToggle<CR>", {noremap=true, silent=true, desc="Toggle sticky scroll (context)"})
+
+
 
 
