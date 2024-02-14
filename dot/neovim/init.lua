e31e0dccc6d6894cf2cae9f8b9b51aa391930101
Jimmy Charnley Kromann <jimmy@charnley.dk>
2024-02-14 10:13:58 +0100

    require gui after setup

--- dot/neovim/init.lua
+++ dot/neovim/init.lua
@@ -2,11 +2,11 @@
 require 'plugins' -- Install all plugins
 require 'base' -- Standard VIM settings
 require 'maps' -- Most key mappings
-require 'gui' -- Everything related to visual
 
 -- Check if vim is being installed for the first time
 local is_not_setup = vim.env.NEOVIM_SETUP
 
 if is_not_setup == nil then
+    require 'gui' -- Everything related to visual
     require 'ide'
 end
