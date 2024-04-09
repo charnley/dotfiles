9ccc5e16154e5c16bafd3c8f4a3caa6028d03244
Jimmy Charnley Kromann <jimmy@charnley.dk>
2024-04-09 15:12:26 +0200

    updated go

--- setup/go_install.sh
+++ setup/go_install.sh
@@ -2,7 +2,10 @@
 
 cd ~/deb/
 
-wget https://go.dev/dl/go1.21.5.linux-amd64.tar.gz
-extract go1.21.5.linux-amd64.tar.gz
+version=$(curl https://go.dev/VERSION?m=text | head -n 1)
+
+wget "https://dl.google.com/go/$version.linux-amd64.tar.gz"
+tar xvf $version.linux-amd64.tar.gz
 
 mv go ~/opt/
+ln -s ~/opt/go/bin/go ~/bin/go
