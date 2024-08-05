bfc1ae4f910741b7197006c43e1b98bffa8b1997
Jimmy Charnley Kromann <jimmy@charnley.dk>
2024-08-05 16:17:30 +0200

    Backward compatiable neovim

--- setup.deb/nvim_setup.sh
+++ setup.deb/nvim_setup.sh
@@ -8,6 +8,11 @@ cd ~/opt
 mkdir -p neovim
 cd neovim
 
+# gcc --version | awk '/gcc/ && ($3+0)>9.1{print "Version is greater than 9.1"}'
+# https://github.com/neovim/neovim-releases/releases/download/v0.10.1/nvim.appimage
+# https://github.com/neovim/neovim-releases/releases/latest/download/nvim.appimage
+
+# curl -LO https://github.com/neovim/neovim-releases/releases/latest/download/nvim.appimage
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 # curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
 chmod u+x nvim.appimage
