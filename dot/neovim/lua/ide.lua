85fb0e7ca8e724514b3b68a644275f7126316956
Jimmy Charnley Kromann <jimmy@charnley.dk>
2025-06-03 12:08:15 +0200

    Better search icon

--- dot/neovim/lua/ide.lua
+++ dot/neovim/lua/ide.lua
@@ -319,8 +319,8 @@ require("noice").setup({
   cmdline = {
       format = {
         cmdline = { icon = ">" },
-        search_down = { icon = "⚲⌄" },
-        search_up = { icon = "⚲⌃" },
+        search_down = { icon = "⌕⌄" },
+        search_up = { icon = "⌕⌃" },
         filter = { icon = "$" },
         lua = { icon = "☾" },
         help = { icon = "?" },
