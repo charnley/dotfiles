71caef9d25e58ed4b8c901691672802072aa8647
Jimmy Charnley Kromann <jimmy@charnley.dk>
2025-06-03 13:38:01 +0200

    fixed neovim install script

--- setup.deb/nvim_setup.sh
+++ setup.deb/nvim_setup.sh
@@ -13,8 +13,11 @@ cd neovim
 # https://github.com/neovim/neovim-releases/releases/latest/download/nvim.appimage
 
 # curl -LO https://github.com/neovim/neovim-releases/releases/latest/download/nvim.appimage
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+#curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim_x86_64.appimage
 # curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
+#wget https://github.com/neovim/neovim/releases/download/v0.11.2/nvim-linux-x86_64.appimage
+wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage
+mv ./nvim-linux-x86_64.appimage nvim.appimage
 chmod u+x nvim.appimage
 
 # ln -s ~/opt/neovim/nvim.appimage ~/bin/vim
