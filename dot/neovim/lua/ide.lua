03f43f5ea3bb4389e586c98d0ef3d96eb2fd5b03
Jimmy Charnley Kromann <jimmy@charnley.dk>
2025-06-03 10:47:43 +0200

    better icon-free ui

--- dot/neovim/lua/ide.lua
+++ dot/neovim/lua/ide.lua
@@ -316,6 +316,28 @@ require("tiny-inline-diagnostic").setup({
 
 -- Better information / popups, instead of having to press "ok"
 require("noice").setup({
+  cmdline = {
+      format = {
+        cmdline = { icon = ">" },
+        search_down = { icon = "⚲⌄" },
+        search_up = { icon = "⚲⌃" },
+        filter = { icon = "$" },
+        lua = { icon = "☾" },
+        help = { icon = "?" },
+      },
+    },
+    format = {
+      level = {
+        icons = {
+          error = "✖",
+          warn = "▼",
+          info = "●",
+        },
+      },
+    },
+    popupmenu = {
+      kind_icons = false,
+    },
   lsp = {
     -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
     override = {
