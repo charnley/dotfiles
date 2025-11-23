1b836d31143fb416ced6863dd6e24731629f379f
Jimmy Charnley Kromann <jimmy@charnley.dk>
2025-11-23 20:33:28 +0100

    Update app image

--- setup.deb/nvim_setup.sh
+++ setup.deb/nvim_setup.sh
@@ -10,14 +10,16 @@ cd neovim
 
 # gcc --version | awk '/gcc/ && ($3+0)>9.1{print "Version is greater than 9.1"}'
 # https://github.com/neovim/neovim-releases/releases/download/v0.10.1/nvim.appimage
-# https://github.com/neovim/neovim-releases/releases/latest/download/nvim.appimage
+wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux-x86_64.appimage
 
 # curl -LO https://github.com/neovim/neovim-releases/releases/latest/download/nvim.appimage
 #curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim_x86_64.appimage
 # curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
 #wget https://github.com/neovim/neovim/releases/download/v0.11.2/nvim-linux-x86_64.appimage
-wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage
-mv ./nvim-linux-x86_64.appimage nvim.appimage
+
+# wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage
+# mv ./nvim-linux-x86_64.appimage nvim.appimage
+
 chmod u+x nvim.appimage
 
 # ln -s ~/opt/neovim/nvim.appimage ~/bin/vim
