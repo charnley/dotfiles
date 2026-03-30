a7c74f83af501fa25468f0bbf7eda4b8533e1dd0
Jimmy Kromann <jimmy@charnley.dk>
2026-03-30 23:52:00 +0200

    Updated native resolution, font size, zsh env sourcing, and Lua version to 5.4.8.

--- setup/install-lua.sh
+++ setup/install-lua.sh
@@ -2,16 +2,17 @@
 
 # TODO Should be 5.1.*
 
-version="5.4.3"
+# https://lua.org/ftp/lua-5.4.8.tar.gz
+version="5.4.8"
 lua_build="$HOME/opt/lua_build"
 
 mkdir -p $lua_build
 cd $lua_build
 
-curl -R -O http://www.lua.org/ftp/lua-$version.tar.gz
+wget http://lua.org/ftp/lua-$version.tar.gz
 tar -zxf lua-$version.tar.gz
 cd lua-$version
 
 make linux test
 
-make install
+# make install
