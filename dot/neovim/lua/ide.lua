3b43569dae70e546513fe111fc3ff11f1901cb40
Jimmy Charnley Kromann <jimmy@charnley.dk>
2026-06-29 11:31:40 +0200

    Removed refactoring plugin keymaps.

--- dot/neovim/lua/ide.lua
+++ dot/neovim/lua/ide.lua
@@ -242,34 +242,6 @@ vim.api.nvim_set_keymap(
   { noremap = true, silent = true, desc = "Toggle sticky scroll (context)" }
 )
 
--- Refactor
-vim.keymap.set("x", "<leader>re", function()
-  require("refactoring").refactor("Extract Function")
-end, { desc = "Extract Function" })
-vim.keymap.set("x", "<leader>rf", function()
-  require("refactoring").refactor("Extract Function To File")
-end, { desc = "Extract function to file" })
--- Extract function supports only visual mode
-vim.keymap.set("x", "<leader>rv", function()
-  require("refactoring").refactor("Extract Variable")
-end, { desc = "Extract variable" })
--- Extract variable supports only visual mode
-vim.keymap.set("n", "<leader>rI", function()
-  require("refactoring").refactor("Inline Function")
-end, { desc = "Inline function" })
--- Inline func supports only normal
-vim.keymap.set({ "n", "x" }, "<leader>ri", function()
-  require("refactoring").refactor("Inline Variable")
-end, { desc = "Inline variable" })
--- Inline var supports both normal and visual mode
-vim.keymap.set("n", "<leader>rb", function()
-  require("refactoring").refactor("Extract Block")
-end, { desc = "Extract block" })
-vim.keymap.set("n", "<leader>rbf", function()
-  require("refactoring").refactor("Extract Block To File")
-end, { desc = "Extract block to file" })
--- Extract block supports only normal mode
-
 -- -- Leap
 -- require("leap").create_default_mappings()
 -- -- Define equivalence classes for brackets and quotes, in addition to
